c4897e3d02cc52a77510bef9816bfd905d25fae8
sython93 <samstringer3rha@hotmail.com>
2024-06-06 15:12:22 +0100

    Adding files to python packages

--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -6,6 +6,4 @@ class HomePage:
         self.stock_dropdown = page.get_by_role("button", name="Stock >")
         self.dynamic_toggle = page.get_by_role("button", name="Dynamic")
         self.ctrlk_button = page.get_by_role("button", name="Ctrl+K")
-        self.sign_in_button = page.get_by_role("button", name="Sign In")
-        self.email = page.get_by_label("email")
-        self.password = page.get_by_label("password")
+
---
+++ pom/login_page_elements.py
@@ -0,0 +1,8 @@
+
+
+class HomePage:
+
+    def __init__(self, page):
+        self.sign_in_button = page.get_by_role("button", name="Sign In")
+        self.email = page.get_by_label("email")
+        self.password = page.get_by_label("password")
